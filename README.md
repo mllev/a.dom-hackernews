5f6d46554e23565c355063ec32edffd0ae109cff
Matt Levenstein <m@pop-os.localdomain>
2023-08-20 13:50:22 -0400

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+## A.DOM Hackernews Clone
+This is a complete A-DOM web application example, with full SSR.
+
+`npm install`
+
+
+`npm run dev`
---
+++ server.js
@@ -0,0 +1,55 @@
+const adom = require('adom-js');
+
+const prod = !process.argv.includes('dev');
+
+const mapStories = {
+  top: "news",
+  new: "newest",
+  show: "show",
+  ask: "ask",
+  job: "jobs",
+};
+
+async function storiesHandler(req) {
+  const page = parseInt(req.query.page || '1');
+  const type = mapStories[req.params.type || 'top'];
+  if (!type) return null;
+  const stories = await adom.request(`https://api.hackerwebapp.com/${type}?page=${page}`);
+  return { stories, page, type: req.params.type };
+}
+
+async function storyHandler(req) {
+  const id = req.params.id;
+  const story = await adom.request(`https://api.hackerwebapp.com/item/${id}`);
+  return { story };
+}
+
+async function userHandler(req) {
+  const id = req.params.id;
+  const user = await adom.request(`https://hacker-news.firebaseio.com/v0/user/${id}.json`);
+  return { user };
+}
+
+adom.serve({
+  publicDir: './public',
+  cache: prod,
+  minify: prod,
+  routes: {
+    '/': {
+      input: './src/index.adom',
+      data: storiesHandler
+    },
+    '/:type': {
+      input: './src/index.adom',
+      data: storiesHandler
+    },
+    '/item/:id': {
+      input: './src/story.adom',
+      data: storyHandler
+    },
+    '/users/:id': {
+      input: './src/user.adom',
+      data: userHandler
+    }
+  },
+});
